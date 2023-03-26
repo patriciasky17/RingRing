3ee1b07222a3695625e853580e50ee488f49e8a3
Patricia Ho <hopatricia1711@gmail.com>
2023-03-26 13:44:15 +0700

    Initial Commit

---
+++ RingRing/RingRingApp.swift
@@ -0,0 +1,17 @@
+//
+//  RingRingApp.swift
+//  RingRing
+//
+//  Created by Patricia Ho on 26/03/23.
+//
+
+import SwiftUI
+
+@main
+struct RingRingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
